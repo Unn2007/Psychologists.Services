f6320dbeb6dca28d05396a9c3ffe8b142da181df
Bua2007 <fopbukachalisa@gmail.com>
2025-01-26 08:46:14 +0200

    init firebase auth

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,16 @@
 import DocumentTitle from '../../components/DocumentTitle';
 import css from './HomePage.module.css'
+// import { createUserWithEmailAndPassword } from "firebase/auth"; 
+// import { auth } from '../../utils/firebaseAuth.js';
 
  const HomePage = ()=> {
+    // createUserWithEmailAndPassword(auth, "unn1000@i.ua", "asd1234")
+    //   .then((user) => {
+    //     console.log(user);
+        
+    //   })
+    //   .catch((error) => console.log(error));
+     
     return (
         <>
         <DocumentTitle>Home</DocumentTitle>
