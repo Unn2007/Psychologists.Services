fbca9f8c87a15ce4e0564e262d93d2d2e97a2bbb
Bua2007 <fopbukachalisa@gmail.com>
2025-01-30 17:20:01 +0200

    add styles and markup for HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,8 @@
 import DocumentTitle from '../../components/DocumentTitle';
-import {Icon} from '../../components/Icon/Icon.jsx';
+import { Icon } from '../../components/Icon/Icon.jsx';
 import css from './HomePage.module.css';
+import mainFoto from '../../assets/images/mainFoto.jpg';
+import mainFoto2x from '../../assets/images/mainFoto2x.jpg';
 // import { useDispatch } from "react-redux";
 // import { logIn } from "../../redux/auth/operations";
 // import { createUserWithEmailAndPassword } from "firebase/auth";
@@ -23,30 +25,64 @@ const HomePage = () => {
   //   .catch((error) => console.log(error));
 
   return (
+    <section className={css.home}>
     <div className={`container ${css.container}`}>
       <DocumentTitle>Home</DocumentTitle>
-      <div>
-        <h1>
-          <span>The road to the</span>
-          <span>depths</span>
-          <span>of the human soul</span>
+      <div className={css.hero}>
+        <h1 className={css.mainHeader}>
+          <span>The road to the </span>
+          <span className={css.italicText}>depths</span>
+          <span> of the human soul</span>
         </h1>
-        <p>
+        <p className={css.text}>
           We help you to reveal your potential, overcome challenges and find a
           guide in your own life with the help of our experienced psychologists.
         </p>
-        <button type="button">
-          <span>Get started</span>
+        <button type="button" className={css.button}>
+          <span className={css.buttonText}>Get started</span>
           <Icon
-            width={16}
-            height={16}
+            width={14}
+            height={18}
             href="icon-arrow"
             className={css.iconArrow}
           />
         </button>
       </div>
-      <div></div>
+      <div className={css.info}>
+        <div className={css.thumb}>
+          <img
+            srcSet={`${mainFoto} 1x, ${mainFoto2x} 2x`}
+            src={mainFoto}
+            alt="Foto woman psychologist"
+            className={css.mainImg}
+          />
+        </div>
+        <div className={css.quantity}>
+          <div className={css.iconWrapper}>
+            <Icon
+              width={20}
+              height={15}
+              href="icon-check"
+              className={css.check}
+            />
+          </div>
+          <p>
+            <span className={css.quantityText}>Experienced psychologists</span>
+            <span className={css.quantityNumber}>15,000</span>
+          </p>
+          
+        </div>
+        <div className={css.question}>
+          <Icon
+              width={9}
+              height={15}
+              href="icon-question"
+              className={css.questionIcon}
+            />
+          </div>
+      </div>
     </div>
+    </section>
   );
 };
 
