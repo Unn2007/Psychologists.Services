86009afff798bce5fbe2282dadc9f2dc1a6e10be
Bua2007 <fopbukachalisa@gmail.com>
2025-03-09 18:32:27 +0200

    update styles and markup for FiltersForm

--- src/components/FiltersForm/FiltersForm.jsx
+++ src/components/FiltersForm/FiltersForm.jsx
@@ -1,13 +1,29 @@
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Icon } from '../Icon/Icon.jsx';
 import { setSelectedOption } from '../../redux/filters/slice.js';
 import css from './FiltersForm.module.css';
 
 export const FiltersForm = () => {
   const dispatch = useDispatch();
-  const { register, watch, setValue } = useForm();
+  const { setValue, watch } = useForm({
+    defaultValues: { option: 'Show all' },
+  });
   const selectedOption = watch('option');
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const options = [
+    'Show all',
+    'A to Z',
+    'Z to A',
+    'Less than 10$',
+    'Greater than 10$',
+    'Popular',
+    'Not popular',
+  ];
 
   useEffect(() => {
     if (selectedOption) {
@@ -15,39 +31,38 @@ export const FiltersForm = () => {
     }
   }, [selectedOption, dispatch]);
 
+  const handleSelect = (option) => {
+    setValue('option', option);
+    setIsOpen(false);
+  };
+
   return (
     <form className={css.form}>
-      <label htmlFor="option" className={css.label}>
-        Filters
-      </label>
-      <select
-        className={css.select}
-        id="option"
-        {...register('option')}
-        onChange={(e) => setValue('option', e.target.value)}
-      >
-        <option className={css.option} value="Show all">
-          Show all
-        </option>
-        <option className={css.option} value="A to Z">
-          A to Z
-        </option>
-        <option className={css.option} value="Z to A">
-          Z to A
-        </option>
-        <option className={css.option} value="Less than 10$">
-          Less than 10$
-        </option>
-        <option className={css.option} value="Greater than 10$">
-          Greater than 10$
-        </option>
-        <option className={css.option} value="Popular">
-          Popular
-        </option>
-        <option className={css.option} value="Not popular">
-          Not popular
-        </option>
-      </select>
+      <label className={css.label}>Filters</label>
+      <div className={css.customSelect} onClick={() => setIsOpen(!isOpen)}>
+        {selectedOption}
+        <Icon
+          width={15}
+          height={10}
+          href={`${isOpen ? 'icon-up' : 'icon-down'}`}
+          className={css.arrow}
+        />
+      </div>
+      {isOpen && (
+        <ul className={css.optionsList}>
+          {options.map((option) => (
+            <li
+              key={option}
+              className={`${css.option} ${
+                selectedOption === option ? css.active : ''
+              }`}
+              onClick={() => handleSelect(option)}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
     </form>
   );
 };
